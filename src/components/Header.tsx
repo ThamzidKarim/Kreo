21a9a6d8e03249a6b3d89ea18b18c13fbe75f674
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-02-10 23:59:40 +0000

    Align Header in top left

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     
     return(
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-blue-500 text-white p-4 absolute top-0 left-0">
         <h1>Kreo</h1>
     </header>
     );
