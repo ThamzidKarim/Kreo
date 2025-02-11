fa998777d5059ebe6fab2b3de58a2c82d51cbca0
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-02-11 23:58:33 +0000

    Add NavBar to top

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,8 @@
 function NavBar() {
     
     return(
-        <nav>
-            <ul>
+        <nav className="bg-blue-500 text-white p-4 w-full fixed top-0 left-0">
+            <ul className="flex gap-6">
                 <li><a href="">Library</a></li>
                 <li><a href="">Story</a></li>
                 <li><a href="">Scenes</a></li>
