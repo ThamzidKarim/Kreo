dcb1608fcee4e01b806cae799c41774cfd0288e2
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-03-29 22:36:05 +0000

    Make Header clickable to navigate to the Home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router";
+
 function Header() {
     
     return(
     <header className="bg-blue-500 text-white p-4 absolute top-0 left-0">
-        <h1>Kreo</h1>
+        <Link to="/">Kreo</Link>
     </header>
     );
 
