b47afbd14f364afc167d8bddb581cc0b414483f4
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-04-10 18:28:15 +0100

    Reposition logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,27 +13,27 @@ function NavBar() {
   const location = useLocation(); // Get current path
 
   return (
-    <nav className="bg-[#F8F5F5] text-[#ffe2e2] p-4 h-full fixed top-0 left-0 w-[310px]">
+    <nav className="bg-[#F8F5F5] text-[#000000] p-4 h-full fixed top-0 left-0 w-[210px]">
       {/* Logo Section */}
       <div className="flex items-center mb-8">
-        <div className="flex flex-col gap-[12.5px] ml-6">
+        <div className="flex flex-col gap-[12.5px] ml-2">
           <div className="w-[60px] h-[1px] bg-black"></div>
           <div className="w-[60px] h-[1px] bg-black"></div>
           <div className="w-[60px] h-[1px] bg-black"></div>
         </div>
-        <div className="text-4xl ml-14 font-inter">KREO</div>
+        <div className="ml-4 text-4xl font-inter">KREO</div>
       </div>
 
       {/* Nav Links */}
-      <div className="flex flex-col gap-2 px-5">
+      <div className="flex flex-col gap-2">
         {NavLinks.map(({ name, path }) => {
           const isActive = location.pathname === path;
 
           return (
             <Link key={name} to={path}>
               <div
-                className={`h-[60px] rounded-lg flex items-center justify-center text-base font-inter transition-colors ${
-                  isActive ? "bg-[#E6E6E6]" : "bg-[#F5F5F5] hover:bg-[#E6E6E6]"}`
+                className={`h-[40px] rounded-lg flex items-center justify-center text-base font-inter transition-colors ${
+                  isActive ? "bg-[#E6E6E6]" : "bg-[#F8F5F5] hover:bg-[#E6E6E6]"}`
                 }
               >
                 {name}
