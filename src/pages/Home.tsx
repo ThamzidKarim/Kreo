ae75ecab1a93cf9660e5d4852f9366b8b92abb6c
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-04-13 23:53:25 +0100

    Add New Project button to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router";
+
 import NavBar from "../components/NavBar.tsx";
 
 function Home() {
     return (
         <div className="min-h-screen bg-white">
             <NavBar />
+            
             <div className="flex justify-center ml-auto min-h-screen pt-[50px]">
-                <div className="relative w-[90%] max-w-[924px] h-[250px] bg-[#EBE7E7] rounded-[46px] left-[100px]">
+            <Link to="/new-project" className="relative w-[90%] max-w-[924px] h-[250px] bg-[#EBE7E7] rounded-[46px] left-[100px] hover:bg-[#adadad] transition-colors cursor-pointer shadow-lg">
                     <div className="absolute w-[60px] h-px left-[20%] top-[50%] border border-black" />
                     <div className="absolute w-[60px] h-px left-[23%] top-[40%] rotate-90 origin-top-left border border-black" />
                     <div className="absolute top-[30%] left-[32%] text-[6vw] md:text-[84px] text-black/60 font-inter font-normal leading-none">
                         New Project
                     </div>
-                </div>
+                </Link>
             </div>
         </div>
     );
