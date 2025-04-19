97b2f7389b944d2d562308112b43d33a53c43ab4
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-04-19 23:58:26 +0100

    Add icons to home page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,13 @@
+import { Bars3Icon, HomeIcon } from "@heroicons/react/24/outline";
 import { Link, useLocation } from "react-router";
 
 const NavLinks = [
-  { name: "Home", path: "/home" },
-  { name: "Library", path: "/library" },
-  { name: "Story", path: "/story" },
-  { name: "Scenes", path: "/scenes" },
-  { name: "Editor", path: "/editor" },
-  { name: "For You", path: "/for-you" },
+  { name: "Home", path: "/home", icon: <HomeIcon className="size-7" />},
+  { name: "Library", path: "/library", icon: <HomeIcon className="size-7" />},
+  { name: "Story", path: "/story", icon: <HomeIcon className="size-7" />},
+  { name: "Scenes", path: "/scenes", icon: <HomeIcon className="size-7" />},
+  { name: "Editor", path: "/editor", icon: <HomeIcon className="size-7" />},
+  { name: "For You", path: "/for-you", icon: <HomeIcon className="size-7" />},
 ];
 
 function NavBar() {
@@ -16,26 +17,25 @@ function NavBar() {
     <nav className="bg-[#F8F5F5] text-[#000000] p-4 h-full fixed top-0 left-0 w-[210px]">
       {/* Logo Section */}
       <div className="flex items-center mb-8">
-        <div className="flex flex-col gap-[12.5px] ml-2">
-          <div className="w-[60px] h-[1px] bg-black"></div>
-          <div className="w-[60px] h-[1px] bg-black"></div>
-          <div className="w-[60px] h-[1px] bg-black"></div>
+        <div className="flex items-center justify-center rounded-full bg-[#F8F5F5] hover:bg-[#adadad] cursor-pointer relative w-[40px] h-[40px]">
+          <Bars3Icon className="size-8" /> 
         </div>
-        <div className="ml-4 text-4xl font-inter">KREO</div>
+        <div className="ml-6 text-4xl font-inter">KREO</div>
       </div>
 
       {/* Nav Links */}
       <div className="flex flex-col gap-2">
-        {NavLinks.map(({ name, path }) => {
+        {NavLinks.map(({ name, path, icon }) => {
           const isActive = location.pathname === path;
 
           return (
             <Link key={name} to={path}>
               <div
-                className={`h-[40px] rounded-lg flex items-center justify-center text-base font-inter transition-colors ${
+                className={`h-[40px] rounded-lg flex items-center justify-start gap-3 px-3 text-base font-inter transition-colors ${
                   isActive ? "bg-[#E6E6E6]" : "bg-[#F8F5F5] hover:bg-[#E6E6E6]"}`
                 }
               >
+                {icon}
                 {name}
               </div>
             </Link>
