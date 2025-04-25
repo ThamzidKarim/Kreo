a5bfbed947ce60c7ceda0ccbe0ba91810f35804a
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-04-25 23:59:23 +0100

    Add generate button to prompt bar

--- frontend/src/components/PromptBar.tsx
+++ frontend/src/components/PromptBar.tsx
@@ -7,6 +7,9 @@ function PromptBar() {
                 className="w-full bg-transparent text-lg px-10 py-6 outline-none" 
                 placeholder="Describe your scenes..." 
             />
+            <div className="flex justify-end mt-[-50px] mr-10">
+                <button className="bg-[#F2F2F2] text-[#000000] rounded-[20px] px-4 py-2 font-semibold">Generate</button>
+            </div>
         </div>
     );
 }
