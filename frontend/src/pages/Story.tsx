86723256daeb2142471349b56109f7e89a1eb989
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-05-04 22:55:15 +0100

    Install Shadcn to project and import Button and Textarea components

--- frontend/src/pages/Story.tsx
+++ frontend/src/pages/Story.tsx
@@ -1,17 +1,34 @@
-import Header from "../components/Header";
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 import PromptBar from "../components/PromptBar";
-import TextInputBox from "../components/TextInputBox";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 
 function Story() {
+    const [text, setText] = useState("");
+
+
     return(
-        <div>
-            <Header />
+        <div className="flex flex-col h-screen">
             <NavBar />
-            
-            <div className="flex flex-col items-center space-y-8 px-4 pt-10">
-                <TextInputBox />
-                <PromptBar />
+            <div className="flex flex-col items-center flex-grow pl-30 pt-30">
+                <div className="grid w-md gap-4">
+                    <Textarea 
+                        placeholder="Type your text here. Max length is 10,000..."
+                        maxLength={10000}
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        className="min-h-100 max-h-100 resize-none"
+                    />
+                    <p className="text-sm text-muted-foreground text-right">
+                        {text.length.toLocaleString()} / 10,000 characters
+                    </p>
+                    <Button>Generate Prompts</Button>
+                </div>
+                <div className="mt-auto mb-10">
+                    <PromptBar />
+                </div>
             </div>
         </div>
     );
