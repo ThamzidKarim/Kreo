3dbb9dfa72ba40c62b9c9586b9316e6620840aef
ThamzidKarim <127969695+ThamzidKarim@users.noreply.github.com>
2025-05-13 14:48:59 +0100

    Create file to process PDFs

---
+++ backend/routes/pdfProcessor.js
@@ -0,0 +1,12 @@
+/*
+ * Author: Thamzid Karim
+ * Date: 13/5/2025
+ *
+ * Handles PDF upload, page range extraction, and prompt generation.
+ */
+
+
+
+// TODO: ROUTES TO HANDLE PDF PROCESSING AND HELPER FUNCTIONS
+import express from "express";
+
